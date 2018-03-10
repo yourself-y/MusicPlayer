96eb167dde747c3f332598f38b090b4d9d84b811
yourself-y <2856836686@qq.com>
2018-03-10 22:38:33 +0800

    add layout

--- musicPlayer.js
+++ musicPlayer.js
@@ -14,11 +14,13 @@ import {
 } from 'react-native'
 import { commonStyle } from './commonStyle'
 import Video from 'react-native-video'
+import Icon from 'react-native-vector-icons/FontAwesome'
 
 const mockData = require('./musicList.json')
 const deviceInfo = {
-  deviceWidth: Dimensions.get('window').width,
-  deviceHeight: Platform.OS === 'ios' ? Dimensions.get('window').height : Dimensions.get('window').height - 24
+	deviceWidth: Dimensions.get('window').width,
+  // deviceHeight: Platform.OS === 'ios' ? Dimensions.get('window').height : Dimensions.get('window').height - 24
+	deviceHeight: Dimensions.get('window').height,
 }
 
 const header = {
@@ -29,47 +31,93 @@ const header = {
 export default class MusicPlayer extends Component {
 	constructor(props) {
 	    super(props)
-		this.rotation = true
+		this.player = ''
+		// this.rotation = true
+		this.musicList = []
 	    this.state = {
-	      currentIndex: 0,
-		  spinValue: new Animated.Value(0),
+			viewRef: null,
+	        paused: false,
+	        duration: 0.00,
+	        slideValue: 0.00,
+	        currentTime: 0.00,
+	        currentIndex: 0,
+	        playMode: 0,
+	        spinValue: new Animated.Value(0),
+	        playIcon: 'play-circle',	//pause-circle
+	        playModeIcon: 'random',		//repeat
+	        musicInfo: {},
 	    }
-		this.spinAnimated = Animated.timing(this.state.spinValue, {
-			toValue: 1,
-			duration: 6000,
-			easing: Easing.inOut(Easing.linear)
-    	})
+		// this.spinAnimated = Animated.timing(this.state.spinValue, {
+		// 	toValue: 1,
+		// 	duration: 6000,
+		// 	easing: Easing.inOut(Easing.linear)
+    	// })
 	}
 
-	spining() {
-	    if (this.rotation) {
-			this.state.spinValue.setValue(0)
-			this.spinAnimated.start(() => {		//????
-				this.spining()
-			})
-	    }
-  	}
+	// spining() {
+	//     if (this.rotation) {
+	// 		this.state.spinValue.setValue(0)
+	// 		this.spinAnimated.start(() => {		//????
+	// 			this.spining()
+	// 		})
+	//     }
+  	// }
+	//
+	// spin() {
+	//     this.rotation = !this.rotation
+	//     if (this.rotation) {
+	// 		this.spinAnimated.start(() => {
+	// 	        this.spinAnimated = Animated.timing(this.state.spinValue, {
+	// 				toValue: 1,
+	// 	          	duration: 6000,
+	// 			  	easing: Easing.inOut(Easing.linear)
+	// 	        })
+	// 			this.spining()
+	//     	})
+	// 	} else {
+	// 		this.state.spinValue.stopAnimation((oneTimeRotate) => {
+	// 	        this.spinAnimated = Animated.timing(this.state.spinValue, {
+	// 		          toValue: 1,
+	// 		          duration: (1 - oneTimeRotate) * 6000,
+	// 		          easing: Easing.inOut(Easing.linear)
+	// 	        })
+	//       	})
+	//   	}
+	// }
+	like() {
+		console.log('click like');
+	}
 
-	spin() {
-	    this.rotation = !this.rotation
-	    if (this.rotation) {
-			this.spinAnimated.start(() => {
-		        this.spinAnimated = Animated.timing(this.state.spinValue, {
-					toValue: 1,
-		          	duration: 6000,
-				  	easing: Easing.inOut(Easing.linear)
-		        })
-				this.spining()
-	    	})
-		} else {
-			this.state.spinValue.stopAnimation((oneTimeRotate) => {
-		        this.spinAnimated = Animated.timing(this.state.spinValue, {
-			          toValue: 1,
-			          duration: (1 - oneTimeRotate) * 6000,
-			          easing: Easing.inOut(Easing.linear)
-		        })
-	      	})
-	  	}
+	download() {
+		console.log('click download');
+	}
+
+	comment() {
+		console.log('click comment');
+	}
+
+	more() {
+		console.log('click more');
+	}
+
+	onChangeMode() {
+		console.log('click mode');
+	}
+
+	preMusic() {
+		console.log('click pre');
+	}
+
+	play() {
+		console.log('click play');
+	}
+
+	nextMusic() {
+		console.log('click next');
+	}
+
+	openMusicList() {
+		console.log('click list');
 	}
 
 	render() {
@@ -77,29 +125,81 @@ export default class MusicPlayer extends Component {
 	    return (
 			<View style={styles.container}>
 				<View style={styles.bgContainer}>
-					<View
-						style={styles.djCard}>
-		 			</View>
-					<Image
-						style={styles.bgCD}
-						source={require('./img/bgCD.png')}
-					/>
-					<Animated.Image
-						style={{
-							width: 170,
-					        height: 170,
-					        borderRadius: 85,
-					        alignSelf: 'center',
-					        position: 'absolute',
-							top: 235,
-					        transform: [{
-								rotate: this.state.spinValue.interpolate({
-									inputRange: [0, 1],
-									outputRange: ['0deg', '360deg']
-					        	})
-							}]}}
-						source={{uri: musicInfo.cover}}
-					/>
+					<View style={styles.djContainer}>
+						<View
+							style={styles.djCard}>
+			 			</View>
+						<Image
+							style={styles.bgCD}
+							source={require('./img/bgCD.png')}
+						/>
+						<Animated.Image
+							style={{
+								width: 170,
+						        height: 170,
+						        borderRadius: 85,
+						        alignSelf: 'center',
+						        position: 'absolute',
+								top: 49,
+						        transform: [{
+									rotate: this.state.spinValue.interpolate({
+										inputRange: [0, 1],
+										outputRange: ['0deg', '360deg']
+						        	})
+								}]}}
+							source={{uri: musicInfo.cover}}
+						/>
+					</View>
+					<View style={{flex: 1}}>
+						<View style={styles.tool}>
+							<TouchableOpacity onPress={() => this.like()}>
+								<Icon name={'heart-o'} size={25} color={commonStyle.white}/>
+							</TouchableOpacity>
+							<TouchableOpacity onPress={() => this.download()}>
+								<Icon name={'download'} size={25} color={commonStyle.white}/>
+							</TouchableOpacity>
+							<TouchableOpacity onPress={() => this.comment()}>
+								<Icon name={'commenting'} size={25} color={commonStyle.white}/>
+							</TouchableOpacity>
+							<TouchableOpacity onPress={() => this.more()}>
+								<Icon name={'ellipsis-v'} size={25} color={commonStyle.white}/>
+							</TouchableOpacity>
+						</View>
+						<View style={styles.progressStyle}>
+							<Text style={{width: 35, fontSize: 11, color: commonStyle.white, marginLeft: 5}}>
+								00:00
+							</Text>
+							<Slider
+								style={styles.slider}
+								maximumValue={this.state.duration}
+								minimumTrackTintColor={commonStyle.themeColor}
+								maximumTrackTintColor={commonStyle.iconGray}
+								step={1}
+							/>
+							<View style={{width: 35, alignItems: 'flex-end', marginRight: 5}}>
+								<Text style={{fontSize: 11, color: commonStyle.white}}>
+									05:00
+								</Text>
+							</View>
+						</View>
+						<View style={styles.toolBar}>
+							<TouchableOpacity onPress={() => this.onChangeMode()}>
+								<Icon name={'random'} size={30} color={commonStyle.white}/>
+							</TouchableOpacity>
+							<TouchableOpacity onPress={() => this.preMusic()}>
+								<Icon name={'step-backward'} size={35} color={commonStyle.white}/>
+							</TouchableOpacity>
+							<TouchableOpacity onPress={() => this.play()}>
+								<Icon name={'play-circle-o'} size={60} color={commonStyle.white}/>
+							</TouchableOpacity>
+							<TouchableOpacity onPress={() => this.nextMusic()}>
+								<Icon name={'step-forward'} size={35} color={commonStyle.white}/>
+							</TouchableOpacity>
+							<TouchableOpacity onPress={() => this.openMusicList()}>
+								<Icon name={'bars'} size={30} color={commonStyle.white}/>
+							</TouchableOpacity>
+						</View>
+					</View>
 					<Video
 						ref={'player'}
 						source={{uri: musicInfo.url}}
@@ -127,28 +227,56 @@ const styles = StyleSheet.create({
 	},
 	bgContainer: {
 		position: 'absolute',
-		left: 0,
-		right: 0,
-		top: 0,
-		bottom: 0,
 		height: deviceInfo.deviceHeight,
 		width: deviceInfo.deviceWidth,
+		backgroundColor: commonStyle.red,
  	},
 	bgCD: {
 		width: 260,
 		height: 260,
 		alignSelf: 'center',
 		position: 'absolute',
-		top: 190
+		top: 5,
 	},
 	djCard: {
 	    width: 270,
 	    height: 270,
-	    marginTop: 185,
 	    borderColor: commonStyle.gray,
 	    borderWidth: 10,
 	    borderRadius: 190,
 	    alignSelf: 'center',
-	    opacity: 0.2
+	    opacity: 0.2,
+	},
+	djContainer: {
+		top: 120,
+	},
+	tool: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'space-around',
+		top: 180,
+	},
+	progressStyle: {
+		position: 'absolute',
+	    flexDirection: 'row',
+	    marginHorizontal: 10,
+	    alignItems: 'center',
+		top: 240,
+	},
+ 	slider: {
+	    flex: 1,
+	},
+	toolBar: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'space-around',
+		top: 245,
+  	},
+	absolute: {
+	    position: "absolute",
+	    top: 0,
+	    left: 0,
+	    bottom: 0,
+	    right: 0,
 	},
 })
